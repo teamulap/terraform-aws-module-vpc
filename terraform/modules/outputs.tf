a4c532d39794d9a1369c37b6971e7bf82bc2e86e
nsantiago2719 <nsantiago2719@gmail.com>
2020-05-01 10:07:47 +0800

    update to terraform v0.12 syntax

--- terraform/modules/outputs.tf
+++ terraform/modules/outputs.tf
@@ -1,15 +1,15 @@
 output "vpc_id" {
-    value = "${aws_vpc.vpc.id}"
+    value = aws_vpc.vpc.id
 }
 output "subnet_ids" {
-    value = "${aws_subnet.subnet.*.id}"
+    value = aws_subnet.subnet.*.id
 }
 output "route_table_ids" {
-  value = "${aws_route_table.rt.*.id}"
+  value = aws_route_table.rt.*.id
 }
 output "route_table_association_ids" {
-  value = "${aws_route_table_association.rta.*.id}"
+  value = aws_route_table_association.rta.*.id
 }
 output "internet_gateway_id" {
-  value = "${aws_internet_gateway.gw.id}"
+  value = aws_internet_gateway.gw.id
 }
